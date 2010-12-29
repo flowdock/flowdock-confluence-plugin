0d9056f6383c7ec47f4373fd7c1eef1ccfb71457
Otto Hilska <otto.hilska@nodeta.fi>
2010-12-29 14:16:35 +0200

    Almost working configuration UI.

---
+++ src/main/java/com/flowdock/plugins/confluence/config/ConfigureFlowdockAction.java
@@ -0,0 +1,40 @@
+package com.flowdock.plugins.confluence.config;
+
+import java.util.List;
+
+import com.atlassian.confluence.core.ConfluenceActionSupport;
+import com.atlassian.confluence.spaces.Space;
+import com.atlassian.confluence.spaces.SpaceManager;
+
+public class ConfigureFlowdockAction extends ConfluenceActionSupport {
+	private static final long serialVersionUID = -5732284806136026378L;
+	private SpaceManager spaceManager = null;
+	private List<Space> spaces;
+	
+	public String input() {
+		this.updateSpacesList();
+		return INPUT;
+	}
+	
+	@Override
+	public String getActionName(String fullClassName) {
+		return "Configure Flowdock integration";
+	}
+
+	@Override
+	public boolean isPermitted() {
+		return true; // TODO PermissionManager.hasPermission(User,Permission,Object)
+	}
+	
+	public void setSpaceManager(SpaceManager manager) {
+		this.spaceManager = manager;
+	}
+	
+	public List<Space> getSpaces() {
+		return this.spaces;
+	}
+	
+	private void updateSpacesList() {
+		this.spaces = this.spaceManager.getAllSpaces();
+	}
+}
