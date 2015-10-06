6027809e09252e88da69ead4b0fc7ecc9b529626
Sampo Verkasalo <sampo@flowdock.com>
2015-10-06 19:13:42 +0300

    Post activities to jari

--- src/main/java/com/flowdock/plugins/confluence/FlowdockConnection.java
+++ src/main/java/com/flowdock/plugins/confluence/FlowdockConnection.java
@@ -4,17 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
+
+import com.atlassian.confluence.json.json.JsonObject;
 
 public class FlowdockConnection {
-	public static void sendApiMessage(Map<String, String> params, String apiKey) {
+	public static void sendApiMessage(JsonObject params, String apiKey) {
 		if (params == null || apiKey == null) {
 			return;
 		}
@@ -29,17 +26,17 @@ public class FlowdockConnection {
 		}
 	}
 	
-	private static void postData(URL apiUrl, Map<String, String> params) throws IOException {
+	private static void postData(URL apiUrl, JsonObject params) throws IOException {
 		HttpURLConnection conn = (HttpURLConnection)apiUrl.openConnection();
 		conn.setRequestMethod("POST");
 		conn.setUseCaches(false);
 		conn.setDoOutput(true);
 		conn.setDoInput(true);
+		conn.setRequestProperty("Content-Type", "application/json");
 		conn.setRequestProperty("User-Agent", "Flowdock Confluence Plugin");
 		
 		OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
-		String postData = mapToPostData(params);
-		out.write(postData);
+		out.write(params.serialize());
 		out.close();
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -48,27 +45,8 @@ public class FlowdockConnection {
 		
 		conn.connect();
 	}
-	
-	private static String mapToPostData(Map<String, String> map) throws UnsupportedEncodingException {
-		Iterator<Entry<String, String>> iter = map.entrySet().iterator();
-		StringBuffer sb = new StringBuffer();
-		while (iter.hasNext()) {
-			Entry<String, String> keyval = iter.next();
-			String key = keyval.getKey();
-			String encodedValue = URLEncoder.encode(keyval.getValue(), "UTF-8");
-			
-			sb.append(key);
-			sb.append("=");
-			sb.append(encodedValue);
-			
-			if (!iter.hasNext()) break;
-			
-			sb.append("&");
-		}
-		return sb.toString();
-	}
-	
+
 	private static URL getApiUrl(String apiKey) throws MalformedURLException {
-		return new URL("https://api.flowdock.com/v1/confluence/" + apiKey);
+		return new URL("https://www.flowdock.com/jari/confluence/messages/" + apiKey);
 	}
 }
